57da658a2420bb9348ca359f15e827ec0f56ea3f
Grant Crofton <grantac@hotmail.com>
2014-11-05 12:30:44 +0000

    Added F# version

---
+++ Grant_Fsharp/PipePacker/Packer.fs
@@ -0,0 +1,30 @@
+﻿module Packer
+
+// Return pipes packed into the minumum number of bins based on binSize.
+// Uses the first-fit algorithm
+let pack binSize pipes =
+    
+    let spaceRemaining bin = binSize - List.sum bin
+
+    // Add a pipe to the collection of bins
+    // Put it in the first bin with enough space, otherwise use a new one
+    let add pipe bins =
+        let rec getPackedBins bins pipeAdded =
+            match pipeAdded,bins with
+            | false, bin::rest -> 
+                match bin |> spaceRemaining with
+                | space when space >= pipe -> (pipe::bin)::(getPackedBins rest true) // bin has space - add pipe
+                | _ -> bin::(getPackedBins rest false) // no space in bin - try the rest
+            | false, [] -> [[pipe]] // no bins left - put pipe in a new one
+            | true, remainingBins -> remainingBins // pipe already packed - nothing to do
+        getPackedBins bins false
+
+    // iterate through the pipes, adding each to the collection of bins
+    let rec fillBins bins remainingPipes =
+        match remainingPipes with
+        | pipe::rest -> fillBins (bins |> add pipe) rest // take the first pipe and add it to the bins
+        | [] -> bins // all pipes packed!
+    
+    let sortedPipes = pipes |> List.sort |> List.rev
+
+    fillBins [] sortedPipes
---
+++ Grant_Fsharp/PipePacker/PackerTests.fs
@@ -0,0 +1,25 @@
+﻿module PackerTests
+
+open Xunit
+open FsUnit
+open Packer
+
+[<Fact>]
+let ``single pipe smaller than bin size gives single bin`` () =
+    pack 10 [10] |> should equal [[10]]
+
+[<Fact>]
+let ``two pipes bigger than bin size gives two bins`` () =
+    pack 10 [6;6] |> should equal [[6];[6]]
+
+[<Fact>]
+let ``two pipes smaller than bin size gives one bin`` () =
+    pack 10 [5;5] |> should equal [[5;5]]
+
+[<Fact>]
+let ``several pipes pack into a reasonable number of bins`` () =
+    pack 10 [1;2;3;4;5;6;7;8;9] |> should equal [[1;9];[2;8];[3;7];[4;6];[5]]
+
+[<Fact>]
+let ``set of pipes which first fit doesn't solve perfectly`` () =
+    pack 10 [2;2;3;4;4;5] |> should equal [[2;4;4];[2;3;5]]
